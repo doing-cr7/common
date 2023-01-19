ebf47b9dad2b26b95cfaf84a90fa6c6ae9ed34eb
yingliming <yingliming@xianlai-inc.com>
2023-01-19 12:21:18 +0800

    new zap bug fix

--- zap.go
+++ zap.go
@@ -11,23 +11,23 @@ var (
 )
 
 func init() {
-	// 日志文件名称
-	filename := "micro.log"
+	//日志文件名称
+	fileName := "micro.log"
 	syncWriter := zapcore.AddSync(
 		&lumberjack.Logger{
-			Filename: filename,
-			MaxSize:  521,
+			Filename: fileName, //文件名称
+			MaxSize:  512,      //MB
 			//MaxAge:     0,
-			MaxBackups: 0,
+			MaxBackups: 0, //最大备份
 			LocalTime:  true,
-			Compress:   true,
+			Compress:   true, //是否启用压缩
 		})
-	// 编码
+	//编码
 	encoder := zap.NewProductionEncoderConfig()
-	// 时间格式
+	//时间格式
 	encoder.EncodeTime = zapcore.ISO8601TimeEncoder
 	core := zapcore.NewCore(
-		//编码器
+		// 编码器
 		zapcore.NewJSONEncoder(encoder),
 		syncWriter,
 		//
@@ -48,7 +48,7 @@ func Debugf(template string, args ...interface{}) {
 }
 
 func Info(args ...interface{}) {
-	logger.Info(args)
+	logger.Info(args...)
 }
 
 func Infof(template string, args ...interface{}) {
@@ -56,7 +56,7 @@ func Infof(template string, args ...interface{}) {
 }
 
 func Warn(args ...interface{}) {
-	logger.Warn(args)
+	logger.Warn(args...)
 }
 
 func Warnf(template string, args ...interface{}) {
@@ -64,11 +64,15 @@ func Warnf(template string, args ...interface{}) {
 }
 
 func Error(args ...interface{}) {
-	logger.Error(args)
+	logger.Error(args...)
 }
 
 func Errorf(template string, args ...interface{}) {
 	logger.Errorf(template, args...)
+}
+
+func DPanic(args ...interface{}) {
+	logger.DPanic(args...)
 }
 
 func DPanicf(template string, args ...interface{}) {
@@ -76,7 +80,7 @@ func DPanicf(template string, args ...interface{}) {
 }
 
 func Panic(args ...interface{}) {
-	logger.Panic(args)
+	logger.Panic(args...)
 }
 
 func Panicf(template string, args ...interface{}) {
@@ -84,7 +88,7 @@ func Panicf(template string, args ...interface{}) {
 }
 
 func Fatal(args ...interface{}) {
-	logger.Fatal(args)
+	logger.Fatal(args...)
 }
 
 func Fatalf(template string, args ...interface{}) {
